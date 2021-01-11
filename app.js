e1801a3ed3562bb2d38f84bffb89893e9f6b5d4f
Maksim Dashkevich <maximkacr7@gmail.com>
2021-01-11 19:43:59 +0300

    create server

---
+++ app.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+let users = [
+    {
+        name: 'Maxim'
+    },
+    {
+        name: 'Ruslan'
+    },
+    {
+        name: 'Anton'
+    }
+];
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/user') {
+        switch (req.method) {
+            case 'GET':
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end(JSON.stringify(users));
+                break;
+            
+            case 'POST':
+                users.push({name: 'UserName'});
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end();
+                break;
+
+            case 'PUT':
+                users = [1, 2, 3];
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end();
+                break;
+
+            default:
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Hello world!');
+                break; 
+        }
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
